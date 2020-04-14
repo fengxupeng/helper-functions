3ad04ec129b27ef379ee82df20bedf6c8392ccd9
Fh <1558823260@qq.com>
2020-04-14 22:46:33 +0800

    optimize

--- demo.php
+++ demo.php
@@ -1,9 +1,12 @@
 <?php
 require_once "./vendor/autoload.php";
 use App\Curl;
+use App\Components\Helper;
 
 
-$h = new \App\components\Helper();
+var_dump(Helper::microt());
+
+$h = new Helper();
 var_dump(get_class($h));
 $c = new Curl();
 var_dump(get_class($c));die;
